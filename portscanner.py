0284ebeadbba59802d5c2a251ef8b6a542cb9479
svenjasutter <svenja.sutter2@gmail.com>
2022-05-28 17:49:39 +0200

    create portscanner

---
+++ portscanner.py
@@ -0,0 +1,35 @@
+import socket #shebang line
+import subprocess
+import sys
+from datetime import datetime
+
+subprocess.call('clear', shell=True) #clears the commadline
+
+# Ask for input
+remoteServer = input("Enter a remote host: ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+print("-" * 60)
+print(f"Please wait, scanning remote host {remoteServerIP}")
+print("-" * 60)
+
+timeBeforeScan = datetime.now()
+try:
+    for port in range(1,1025):
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print(f"Port {port}: open")
+        sock.close()
+
+except KeyboardInterrupt:
+    print("You pressed Ctrl+C")
+    sys.exit()
+
+except socket.gaierror:
+    print ("Hostname could not be resolved. Exiting")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
