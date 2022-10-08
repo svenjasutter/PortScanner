ee9d2f4f3ee906bced186a81ec055eab702d2041
svenjasutter <43755867+svenjasutter@users.noreply.github.com>
2022-10-08 10:09:11 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -16,7 +16,7 @@ print("-" * 60)
 
 timeBeforeScan = datetime.now()
 try:
-    for port in range(1,1025):
+    for port in range(1,6025):
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
